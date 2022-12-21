fa897fab045de04c91b975b64eb5b2eae34cd22c
Shivali Kalathiya <shivalikalathiya48@gmail.com>
2022-12-21 11:28:55 +0530

    First Commit

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    LottieAnimationView animationView;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        animationView = findViewById(R.id.animationView);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                animationView.playAnimation();
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },5000);
+    }
+}
