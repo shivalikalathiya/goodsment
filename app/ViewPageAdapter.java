90e25857e97f06a665fe90fdb219a17db45eaaab
Shivali Kalathiya <shivalikalathiya48@gmail.com>
2022-12-28 11:02:32 +0530

    create

---
+++ app/ViewPageAdapter.java
@@ -0,0 +1,2 @@
+public class ViewPageAdapter {
+}
--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 animationView.playAnimation();
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                startActivity(new Intent(SplashActivity.this,login.class));
                 finish();
             }
         },5000);
