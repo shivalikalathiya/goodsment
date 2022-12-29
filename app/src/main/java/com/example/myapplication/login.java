468f6c591d36977e6e36c7007524f0cd15bd9966
Shivali Kalathiya <shivalikalathiya48@gmail.com>
2022-12-29 12:39:27 +0530

    create

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -5,21 +5,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends Activity {
+
+    EditText edtemail,edtpassword;
+    Button login;
+    TextView signup,txtforgotpassword;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mAuth = FirebaseAuth.getInstance();
         Button signin=findViewById(R.id.signin);
+        edtemail = findViewById(R.id.edtemail);
+        edtpassword = findViewById(R.id.edtpassword);
+        signup = findViewById(R.id.signup);
+        txtforgotpassword = findViewById(R.id.txtforgotpassword);
 
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(login.this,MainActivity.class));
+                String email = edtemail.getText().toString();
+                String password = edtpassword.getText().toString();
             }
         });
+
     }
 }
--- app/src/main/java/com/example/myapplication/signup.java
+++ app/src/main/java/com/example/myapplication/signup.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,7 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity {
@@ -24,7 +29,37 @@ public class signup extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         edtemail = findViewById(R.id.edtemail);
         edtpassword = findViewById(R.id.edtpassword);
-        btnSignup - findViewById(R.id.btnSignup);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtemail.getText().toString();
+                String password = edtpassword.getText().toString();
+
+                //Authentication
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(signup.this,login.class));
+                            Toast.makeText(signup.this,"Registration Successful!",Toast.LENGTH_LONG).show();
+
+                        }else {
+                            Toast.makeText(signup.this,"Registration Failed..", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        txtSignin = findViewById(R.id.txtSignin);
+
+        txtSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(signup.this,login.class));
+            }
+        });
 
 
        ;
